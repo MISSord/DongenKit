933d36e3961d9f5595943bd63499b40b8c3175f6
MissOrd <49863600+MISSord@users.noreply.github.com>
2021-05-23 21:20:43 +0800

    第一次上传
    把两个小项目搞在一起

---
+++ zhiyuanShow/Assets/BaseData/BaseData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseData 
+{
+    public static readonly Vector3 gunLocalPosition = new Vector3(-0.06f, -0.02f, 0);
+    public static readonly Vector3 gunScale = new Vector3(0.23f, 0.23f, 0.23f);
+
+    public static float bulletMinDamage = 10;
+    public static float bulletMaxDamage = 20;
+}
---
+++ zhiyuanShow/Assets/DungeonKit/Scripts/Weapon/GunWeapon/Lasergun.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DungeonKIT;
+
+public class Lasergun : Gun
+{
+    private GameObject effect;
+    private LineRenderer laser;
+    private bool isShooting;
+
+    public override void Init()
+    {
+        base.Init();
+        laser = muzzlePos.GetComponent<LineRenderer>();
+        effect = transform.Find("Effect").gameObject;
+    }
+
+    public override void Shoot()
+    {
+
+        if (InputManager.AttackDown)
+        {
+            isShooting = true;
+            laser.enabled = true;
+            effect.SetActive(true);
+        }
+        if (InputManager.AttackUp)
+        {
+            isShooting = false;
+            laser.enabled = false;
+            effect.SetActive(false);
+        }
+        animator.SetBool("Shoot", isShooting);
+
+        if (isShooting)
+        {
+            Fire();
+        }
+    }
+
+    protected override void Fire()
+    {
+        RaycastHit2D hit2D = Physics2D.Raycast(muzzlePos.position, direction, 30);
+
+        // Debug.DrawLine(muzzlePos.position, hit2D.point);
+        laser.SetPosition(0, muzzlePos.position);
+        laser.SetPosition(1, hit2D.point);
+
+        effect.transform.position = hit2D.point;
+        effect.transform.forward = -direction;
+    }
+}
